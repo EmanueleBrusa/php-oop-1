8ad29354809d85c3e93f589b809cfe0fc7147b2c
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-09 17:27:32 +0200

    primo test esercizio base

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
-   
+    //creo una classe con le sue istanze
+    class Movie {
+        public $title;
+        public $genre;
+        public $rating;
+
+        //creo un costruttore
+        public function __construct($title, $genre, $rating){
+            $this->title = $title;
+            $this->genre = $genre;
+            $this->rating = $rating;
+        }
+
+        //creo un metodo
+        //public function {
+
+        //}
+    }
+
+
+    //creo due oggetti movie
+    $movie1 = new Movie ("Il Signore degli Anelli", "Fantasy", 9.9);
+    $movie2 = new Movie ("Inception", "Thriller", 8.7);
 ?>
 
 <!DOCTYPE html>
