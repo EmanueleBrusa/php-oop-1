07bfd477b2b572fbc12238acb0612c0261098bd0
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-10 09:47:06 +0200

    struttura php completa

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
 <?php
+    class Genre {
+        public $name;
+
+        public function __costruct($name){
+            $this->name = $name;
+        }
+    }
     //creo una classe con le sue istanze
     class Movie {
         public $title;
+        public $year;
         public $genre;
+        public $length;
         public $rating;
 
         //creo un costruttore
-        public function __construct($title, $genre, $rating){
+        public function __construct($title, $year, Genre $genre, $length){
             $this->title = $title;
+            $this->year = $year;
             $this->genre = $genre;
-            $this->rating = $rating;
+            $this->length = $length;
         }
 
         //creo un metodo
-        //public function {
+        public function getMovieDetails(){
+            $string = "Movie: ".$this->title.", Genre: ".$this->genre.", Year: ".$this->year.", Length: ".$this->length;
 
-        //}
+            if($this->rating != NULL){
+                $string.= ", Rating: ".$this->rating;
+            }
+
+            return $string;
+        }
     }
 
 
@@ -29,6 +45,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--bootstrap-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-3dZ9wIrMMij8rOH7X3kLfXAzwtcHpuYpEgQg1OA4QAob1e81H8ntUQmQm3pBudqIoySO5j0tHN4ENzA6+n2r4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>oop</title>
 </head>
 <body>
